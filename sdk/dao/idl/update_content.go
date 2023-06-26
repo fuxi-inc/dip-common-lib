327930320b308de7c71cede1ed0bce6f5fd3e54e
Liu Yanglong <liuyanglong@fxzk.org>
2023-06-26 16:19:02 +0800

    add dao idl

---
+++ sdk/dao/idl/update_content.go
@@ -0,0 +1,15 @@
+package idl
+
+import "github.com/fuxi-inc/dip-common-lib/sdk/dis/idl"
+
+type UpdateDataContentRequest struct {
+	Doi          string         `json:"doi,omitempty" binding:"required"`
+	DwDoi        string         `json:"dw_doi" binding:"required"`                 //数据所有者身份标识
+	Signature    string         `json:"signature" binding:"required"`              //DW的私钥签名
+	Content      []byte         `json:"content" binding:"required"`                //数据内容
+	Digest       *idl.ApiDigest `json:"digest,omitempty" binding:"required"`       // 数据内容摘要
+	Confirmation string         `json:"confirmation,omitempty" binding:"required"` // 确权信息。DW私钥对数据摘要的签名
+	SecretKey    string         `json:"secret_key"`                                //文件内容的加密秘钥，当为空时，代表文件内容没有加密
+}
+
+type UpdateDataContentResponseData struct{}
