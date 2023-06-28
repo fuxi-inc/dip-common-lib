487f26230e90dad815872466d0568908fa95b683
Liu Yanglong <liuyanglong@fxzk.org>
2023-06-28 10:17:37 +0800

    add dip-dns sdk

--- sdk/dip-dns/client.go
+++ sdk/dip-dns/client.go
@@ -1,6 +1,11 @@
 package dip_dns
 
-import "go.uber.org/zap"
+import (
+	"github.com/fuxi-inc/dip-common-lib/utils/converter"
+	"github.com/miekg/dns"
+	"go.uber.org/zap"
+	"strings"
+)
 
 /**
 *  请求方式类如：
@@ -27,4 +32,44 @@ func (c *Client) InitLogger(logger *zap.Logger) *Client {
 func (c *Client) InitDnsHost(host string) *Client {
 	c.DnsHost = host
 	return c
+}
+
+func (c *Client) GetPublicKey(identifier string) string {
+	if identifier == "" {
+		return ""
+	}
+	identifier = dns.Fqdn(identifier)
+
+	c.Logger.Info("[GetPublicKey]receive query user key", zap.String("identifier", identifier))
+
+	qtype := dns.TypeCERT
+	req := new(dns.Msg)
+	req.SetQuestion(identifier, qtype)
+	req.SetEdns0(4096, false)
+	msg, err := dns.Exchange(req, c.DnsHost)
+	if err != nil {
+		c.Logger.Error("[GetPublicKey] dns.Exchange error", zap.String("error", err.Error()))
+		return ""
+	}
+	if msg == nil {
+		// 失败：DNS解析无结果
+		c.Logger.Error("[GetPublicKey]failed to handle the request", zap.String("req", converter.ToString(req)))
+		return ""
+	}
+
+	if len(msg.Answer) == 0 {
+		c.Logger.Error("[GetPublicKey]failed to find the public-key", zap.String("identity_identifier", identifier))
+		return ""
+	}
+	a := msg.Answer[0]
+
+	tmp := strings.TrimPrefix(a.String(), a.Header().String())
+	slice := strings.Split(tmp, " ")
+	if len(slice) != 4 {
+		// 失败：无法从结果RR中获取用户公钥
+		c.Logger.Error("[GetPublicKey] failed to split the public-key from the answer RR", zap.String("answer", tmp))
+		return ""
+	}
+
+	return slice[3]
 }
