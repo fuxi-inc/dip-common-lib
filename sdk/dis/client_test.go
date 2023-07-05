ccaef1b578588398715c94653f480b9b5cf9411d
FeiFei <18831608094@163.com>
2023-07-05 14:49:37 +0800

    dis sdk test update

--- sdk/dis/client_test.go
+++ sdk/dis/client_test.go
@@ -1,6 +1,9 @@
 package dis
 
 import (
+	"encoding/base64"
+	"encoding/pem"
+	"os"
 	"testing"
 
 	"github.com/fuxi-inc/dip-common-lib/IDL"
@@ -10,101 +13,220 @@ import (
 	"go.uber.org/zap"
 )
 
-// func Test_DOCreate(t *testing.T) {
+func GetPubKeyString() string {
+	file, err := os.Open("../testpkg/user/alice/public.pem")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	//读取文件的内容
+	info, _ := file.Stat()
+	buf := make([]byte, info.Size())
+	file.Read(buf)
+	//pem解码
+	block, _ := pem.Decode(buf)
+	keybase64 := base64.StdEncoding.EncodeToString(block.Bytes)
+	return keybase64
 
-// 	request := &idl.ApiDOCreateRequest{
-// 		Doi:       "2d8a2384-a705-4c16-a927-1a1b16345b67.viv.cn",
-// 		DwDoi:     "usera.viv.cn",
-// 		PubKey:    "XXX",
-// 		WhoisData: nil,
-// 		Sign:      "XXX",
-// 	}
+}
+func GetPrivKeyString() string {
+	file, err := os.Open("../testpkg/user/alice/private.pem")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	//读取文件的内容
+	info, _ := file.Stat()
+	buf := make([]byte, info.Size())
+	file.Read(buf)
+	//pem解码
+	block, _ := pem.Decode(buf)
+	keybase64 := base64.StdEncoding.EncodeToString(block.Bytes)
+	return keybase64
 
-// 	println(request)
+}
+func Test_DOCreate(t *testing.T) {
+	sign := IDL.SignatureData{}
+	sign.OperatorDoi = "alice.viv.cn"
+	sign.SignatureNonce = "123456"
+	Signature, err := sign.CreateSignature(GetPrivKeyString())
+	assert.Nil(t, err)
+	sign.Signature = Signature
+	request := &idl.ApiDOCreateRequest{
+		Doi:           "2d8a2384-a705-4c16-a927-1a1b16345b67.viv.cn",
+		DwDoi:         "alice.viv.cn",
+		PubKey:        GetPubKeyString(),
+		WhoisData:     nil,
+		SignatureData: sign,
+	}
+	client := NewClient().
+		InitLogger(zap.NewExample()).
+		// TODO: 添加disq的host名称
+		InitDisQ("xxxxx")
 
-// }
+	// 执行被测试的函数
+	ctx := &gin.Context{}
+	response, err := client.ApiDOCreate(ctx, request)
 
-// func Test_DOUpdate(t *testing.T) {
+	// 断言函数返回的错误为 nil
+	assert.Nil(t, err)
 
-// 	// 所有未赋值的字段均为空
+	// 判断 Errno 是否为 0
+	assert.Equal(t, IDL.RespCodeType(0), response.Errno)
 
-// 	// 更新数据标识
-// 	request := &idl.ApiDOUpdateRequest{
-// 		Doi:    "2d8a2384-a705-4c16-a927-1a1b16345b67.viv.cn",
-// 		NewDoi: "XXX.viv.cn",
-// 		DwDoi:  "usera.viv.cn",
-// 		Sign:   "XXX",
-// 	}
+}
 
-// 	// 更新公钥
-// 	request = &idl.ApiDOUpdateRequest{
-// 		Doi:    "2d8a2384-a705-4c16-a927-1a1b16345b67.viv.cn",
-// 		PubKey: "XXX",
-// 		DwDoi:  "usera.viv.cn",
-// 		Sign:   "XXX",
-// 	}
+func Test_DOUpdate(t *testing.T) {
 
-// 	// 更新数据地址及摘要
+	sign := IDL.SignatureData{}
+	sign.OperatorDoi = "alice.viv.cn"
+	sign.SignatureNonce = "123456"
+	Signature, err := sign.CreateSignature(GetPrivKeyString())
+	assert.Nil(t, err)
+	sign.Signature = Signature
 
-// 	digest := &idl.DataDigest{
-// 		Algorithm: "SHA256",
-// 		Result:    "XXX",
-// 	}
+	// 更新数据标识
+	request := &idl.ApiDOUpdateRequest{
+		Doi:    "2d8a2384-a705-4c16-a927-1a1b16345b67.viv.cn",
+		NewDoi: "XXX.viv.cn",
+		DwDoi:  "alice.viv.cn",
 
-// 	classgrade := &idl.ClassificationAndGrading{
-// 		Class: 1024,
-// 		Grade: 2048,
-// 	}
+		SignatureData: sign,
+	}
+	client := NewClient().
+		InitLogger(zap.NewExample()).
+		// TODO: 添加disq的host名称
+		InitDisQ("xxxxx")
 
-// 	auth := &idl.DataAuthorization{
-// 		Confirmation: "XXX",
-// 	}
+	// 执行被测试的函数
+	ctx := &gin.Context{}
+	response, err := client.ApiDOUpdate(ctx, request)
 
-// 	request = &idl.ApiDOUpdateRequest{
-// 		Doi:                      "2d8a2384-a705-4c16-a927-1a1b16345b67.viv.cn",
-// 		Dar:                      "resource.example.com/path",
-// 		Digest:                   digest,
-// 		DwDoi:                    "usera.viv.cn",
-// 		Authorization:            &[]idl.DataAuthorization{*auth}, // 更新其中的Conformation确权信息
-// 		ClassificationAndGrading: classgrade,
-// 		Sign:                     "XXX",
-// 	}
+	// 断言函数返回的错误为 nil
+	assert.Nil(t, err)
 
-// 	// 数据所有者更新自己的权益
+	// 判断 Errno 是否为 0
+	assert.Equal(t, IDL.RespCodeType(0), response.Errno)
+	// 更新公钥
+	request = &idl.ApiDOUpdateRequest{
+		Doi:           "XXX.viv.cn",
+		PubKey:        GetPubKeyString(),
+		DwDoi:         "usera.viv.cn",
+		SignatureData: sign,
+	}
+	response, err = client.ApiDOUpdate(ctx, request)
 
-// 	desc := &idl.PermissionDescription{
-// 		PermissionDoi: "XXX.viv.cn",
-// 		CreatorDoi:    "yyy.viv.cn",
-// 	}
+	// 断言函数返回的错误为 nil
+	assert.Nil(t, err)
 
-// 	auth = &idl.DataAuthorization{
-// 		Type:        0,
-// 		Description: desc,
-// 	}
+	// 判断 Errno 是否为 0
+	assert.Equal(t, IDL.RespCodeType(0), response.Errno)
+	// 更新数据地址及摘要
 
-// 	request = &idl.ApiDOUpdateRequest{
-// 		Doi:           "2d8a2384-a705-4c16-a927-1a1b16345b67.viv.cn",
-// 		Authorization: &[]idl.DataAuthorization{*auth}, // 更新其中的Type和Description
-// 		Sign:          "XXX",
-// 	}
+	digest := &idl.DataDigest{
+		Algorithm: "SHA256",
+		Result:    "sha256",
+	}
 
-// 	// 更新联系方式
+	classgrade := &idl.ClassificationAndGrading{
+		Class: 1024,
+		Grade: 2048,
+	}
 
-// 	whois := &idl.RegistrationData{
-// 		Doi:     "xxx.viv.cn",
-// 		Contact: []string{"xxx", "yyy"},
-// 	}
+	auth := &idl.DataAuthorization{
+		Confirmation: "XXX",
+	}
 
-// 	request = &idl.ApiDOUpdateRequest{
-// 		Doi:       "2d8a2384-a705-4c16-a927-1a1b16345b67.viv.cn",
-// 		WhoisData: whois,
-// 		Sign:      "XXX",
-// 	}
+	request = &idl.ApiDOUpdateRequest{
+		Doi:                      "XXX.viv.cn",
+		Dar:                      "resource.example.com/path",
+		Digest:                   digest,
+		DwDoi:                    "usera.viv.cn",
+		Authorization:            auth, // 更新其中的Conformation确权信息
+		ClassificationAndGrading: classgrade,
+		SignatureData:            sign,
+	}
+	response, err = client.ApiDOUpdate(ctx, request)
 
-// 	println(request)
+	// 断言函数返回的错误为 nil
+	assert.Nil(t, err)
 
-// }
+	// 判断 Errno 是否为 0
+	assert.Equal(t, IDL.RespCodeType(0), response.Errno)
+	// 数据所有者更新自己的权益
 
+	desc := &idl.PermissionDescription{
+		PermissionDoi: "XXX.viv.cn",
+		CreatorDoi:    "yyy.viv.cn",
+	}
+
+	auth = &idl.DataAuthorization{
+		Type:        0,
+		Description: desc,
+	}
+
+	request = &idl.ApiDOUpdateRequest{
+		Doi:           "XXX.viv.cn",
+		Authorization: auth, // 更新其中的Type和Description
+		SignatureData: sign,
+	}
+	response, err = client.ApiDOUpdate(ctx, request)
+
+	// 断言函数返回的错误为 nil
+	assert.Nil(t, err)
+
+	// 判断 Errno 是否为 0
+	assert.Equal(t, IDL.RespCodeType(0), response.Errno)
+	// 更新联系方式
+
+	whois := &idl.RegistrationData{
+		Doi:     "XXX.viv.cn",
+		Contact: []string{"xxx", "yyy"},
+	}
+
+	request = &idl.ApiDOUpdateRequest{
+		Doi:           "XXX.viv.cn",
+		WhoisData:     whois,
+		SignatureData: sign,
+	}
+	response, err = client.ApiDOUpdate(ctx, request)
+
+	// 断言函数返回的错误为 nil
+	assert.Nil(t, err)
+
+	// 判断 Errno 是否为 0
+	assert.Equal(t, IDL.RespCodeType(0), response.Errno)
+
+}
+func Test_DODelete(t *testing.T) {
+	sign := IDL.SignatureData{}
+	sign.OperatorDoi = "alice.viv.cn"
+	sign.SignatureNonce = "123456"
+	Signature, err := sign.CreateSignature(GetPrivKeyString())
+	assert.Nil(t, err)
+	sign.Signature = Signature
+
+	// 删除数据标识
+	request := &idl.ApiDODeleteRequest{
+		Doi: "XXX.viv.cn",
+
+		SignatureData: sign,
+	}
+	client := NewClient().
+		InitLogger(zap.NewExample()).
+		// TODO: 添加disq的host名称
+		InitDisQ("xxxxx")
+
+	// 执行被测试的函数
+	ctx := &gin.Context{}
+	response, err := client.ApiDODelete(ctx, request)
+
+	// 断言函数返回的错误为 nil
+	assert.Nil(t, err)
+
+	// 判断 Errno 是否为 0
+	assert.Equal(t, IDL.RespCodeType(0), response.Errno)
+}
 func Test_DOQuery(t *testing.T) {
 
 	// 设置测试数据
