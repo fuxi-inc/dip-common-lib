8c0d9e5b46098f09599e70fc218a1c0f832a9c2a
Liu Yanglong <liuyanglong@fxzk.org>
2023-07-26 20:10:01 +0800

    add BatchRegisterDataRequest

--- sdk/dao/idl/register_data.go
+++ sdk/dao/idl/register_data.go
@@ -24,3 +24,24 @@ func (s *RegisterDataRequest) ToString() string {
 }
 
 type RegisterDataResponseData struct{}
+
+type RegisterData struct {
+	Doi                      string                        `json:"doi,omitempty" binding:"required"`
+	DwDoi                    string                        `json:"dw_doi" binding:"required"` //数据所有者身份标识
+	PubKey                   string                        `json:"pub_key" binding:"required"`
+	Content                  string                        `json:"content"`                                   //数据内容
+	FilePath                 string                        `json:"file_path" binding:"required"`              //保存的文件路径
+	Digest                   *idl.DataDigest               `json:"digest,omitempty" binding:"required"`       // 数据内容摘要
+	Confirmation             string                        `json:"confirmation,omitempty" binding:"required"` // 确权信息。DW私钥对数据摘要的签名
+	SecretKey                string                        `json:"secret_key"`                                //文件内容的加密秘钥，当为空时，代表文件内容没有加密
+	ClassificationAndGrading *idl.ClassificationAndGrading `json:"classification_grading,omitempty"`          // 数据分类分级信息
+}
+
+type BatchRegisterDataRequest struct {
+	BatchData []*RegisterData `json:"batch_data,omitempty" binding:"required"`
+	IDL.SignatureData
+}
+
+func (s *BatchRegisterDataRequest) ToString() string {
+	return converter.ToString(s)
+}
