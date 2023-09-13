b8fcd4ae1bb8c3bb607444cc64dcabbbb67ed3b4
Liu Yanglong <liuyanglong@fxzk.org>
2023-09-13 15:11:29 +0800

    add test case

--- sdk/dao/update_content_test.go
+++ sdk/dao/update_content_test.go
@@ -63,6 +63,36 @@ func TestClient_UpdateContent(t *testing.T) {
 			},
 			wantErr: false,
 		},
+
+		{
+			name: "[应用测试用户-代理修改权限] 代理更新内容",
+			fields: fields{
+				Logger:   zap.NewExample(),
+				DisHost:  "http://39.107.180.231:8991",
+				DisQHost: "http://39.107.180.231:8053",
+				DaoHost:  "http://127.0.0.1:8990",
+			},
+			args: args{
+				ctx: &gin.Context{},
+				request: &idl.UpdateDataContentRequest{
+					Doi:     "update_user_a_file_aa.viv.cn.",
+					DwDoi:   "update_user_a.viv.cn.",
+					Content: (subjectNewContent),
+					Digest: &idl2.DataDigest{
+						Algorithm: "SHA256",
+						Result:    base64.StdEncoding.EncodeToString(security.Sha256Hash([]byte(subjectNewContent))),
+					},
+					Confirmation: func() string {
+						sign, err := IDL.NewSignatureData().SetOperator("").SetNonce(base64.StdEncoding.EncodeToString(security.Sha256Hash([]byte(subjectNewContent)))).CreateSignature(string(testpkg.GetMockDataContent("/mock_data/user/alice/private.hex")))
+						fmt.Println("SignByPK-->:", sign, err)
+						return sign
+					}(),
+					SecretKey:     "",
+					SignatureData: *IDL.NewSignatureDataWithSign("update_user_b.viv.cn.", string(testpkg.GetMockDataContent("/mock_data/user/alice/private.hex"))),
+				},
+			},
+			wantErr: false,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
