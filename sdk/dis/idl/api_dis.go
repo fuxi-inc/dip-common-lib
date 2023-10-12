1cf291c57cea7946f66893496581916b413e4f4e
liujian <liujian2880@dingtalk.com>
2023-10-12 15:51:35 +0800

    add confirm ownership

--- sdk/dis/idl/api_dis.go
+++ sdk/dis/idl/api_dis.go
@@ -84,6 +84,16 @@ type ApiExchangeOwnershipRequest struct {
 	IDL.SignatureData                   //统一共用的加签验签结构，字段均为必填项
 }
 
+// 交换所有权确认
+type ApiExchangeOwnershipResponse struct {
+	DataDoi           string            `json:"data_doi,omitempty"`
+	DuKey             string            `json:"du_key,omitempty"`        // du的public_key加密
+	Authorization     DataAuthorization `json:"authorization,omitempty"` // 授权信息数组
+	Dar               string            `json:"dar,omitempty"`           // 数据在DAO中的存储地址
+	Fields            map[string]string `json:"fields,omitempty"`        // 扩展字段，用于发送通知
+	IDL.SignatureData                   //统一共用的加签验签结构，字段均为必填项
+}
+
 // 授权发起
 type ApiAuthInitRequest struct {
 	DataDoi           string            `json:"data_doi,omitempty"`
